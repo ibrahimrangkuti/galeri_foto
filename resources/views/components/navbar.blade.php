a8c062cda547024594f4a2a7bba1f6b2c4f70ace
ibrahimrangkuti <rangkutyibrahim765@gmail.com>
2024-01-25 08:07:01 +0700

    initial commit

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg bg-primary navbar-dark py-3">
+    <div class="container">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav me-auto">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Pricing</a>
+                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            </div>
+            <a href="{{ route('login') }}" class="btn btn-info text-white">Login</a>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/pages/register.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="d-flex justify-content-center">
+        <div class="w-25 mt-5 rounded p-4 shadow-lg">
+            <h1 class="text-center mb-5">Register</h1>
+            <form action="">
+                @csrf
+                <div class="mb-3">
+                    <label for="nama" class="form-label">Nama</label>
+                    <input type="text" class="form-control" id="nama" placeholder="Masukkan Nama">
+                </div>
+                <div class="mb-3">
+                    <label for="nis" class="form-label">NIS</label>
+                    <input type="number" class="form-control" id="nis" placeholder="Masukkan NIS">
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email" placeholder="Masukkan Email">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="Masukkan Password">
+                </div>
+                <button type="submit" class="btn btn-primary w-100 mb-3 mt-3">Login</button>
+                <p class="text-center mb-0">Sudah punya akun? <a href="/login" class="text-decoration-none">masuk</a>
+                </p>
+            </form>
+        </div>
+    </div>
+@endsection
